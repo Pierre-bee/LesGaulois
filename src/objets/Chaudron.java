98852dd93d2e6e2494fba81a6dfd8910baa211cb
Pierre Bouzerma <bzp5256a@ensfsi.onmicrosoft.com>
2026-03-17 09:28:29 +0100

    ajout de parler (druide), prendreParole (druide), getNom (druide) et resterPotion (chaudron)

--- src/objets/Chaudron.java
+++ src/objets/Chaudron.java
@@ -1,5 +1,17 @@
 package objets;
 
 public class Chaudron {
-
+	private int quantitePotion;
+	private int forcePotion;
+	
+	public boolean resterPotion() {
+		if (quantitePotion == 0) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	
 }
--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -6,4 +6,16 @@ public class Druide {
 	private String nom;
 	private int force;
 	private Chaudron chaudron;
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+	
+	private String prendreParole() {
+		return "Le Druide " + nom + " : ";
+	}
+	
+	public String getNom() {
+		return nom;
+	}
 }
