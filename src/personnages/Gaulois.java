cae65885e63bce89a08eaa645daef571342f0dbf
Pierre Bouzerma <bzp5256a@ensfsi.onmicrosoft.com>
2026-03-17 09:12:29 +0100

    ajout de recevoir_coup (romain)

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,6 +3,7 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	private int effetPotion = 1;
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,14 +1,21 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		Gaulois obelix = new Gaulois("Obélix", 16);
+		Romain minus = new Romain("Minus", 6);
 		
 		asterix.parler("Bonjour " + obelix.getNom());
 		obelix.parler("Bonojur " + asterix.getNom() + ". Ca te dirais d'aller chasser des sangliers ?");
 		asterix.parler("Oui, très bonne idée");
+		
+		System.out.println("Dans la forêt, " + asterix.getNom() + " et " + obelix.getNom() + " tombent nez à nez sur le romain " + minus.getNom());
+		for (int i = 0; i < 3; i+=1) {
+			asterix.frapper(minus);
+		}
 	}
 }
